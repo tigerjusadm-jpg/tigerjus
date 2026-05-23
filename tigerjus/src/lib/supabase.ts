ac381fb869e2972d767dd293f7a3c50459f14020
tigerjusadm-jpg <tigerjusadm@gmail.com>
2026-05-23 14:06:18 -0400

    Update supabase.ts

--- tigerjus/src/lib/supabase.ts
+++ tigerjus/src/lib/supabase.ts
@@ -1,16 +1,10 @@
+import { createBrowserClient } from '@supabase/ssr'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    flowType: 'pkce',
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
-  }
-})
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
 
 export const supabaseAdmin = () =>
   createClient(supabaseUrl, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
