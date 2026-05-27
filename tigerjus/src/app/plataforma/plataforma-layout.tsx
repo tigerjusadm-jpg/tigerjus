227767e471e01c062f44af052ca8ca1e43058e19
tigerjusadm-jpg <tigerjusadm@gmail.com>
2026-05-27 15:40:01 -0400

    Delete tigerjus/src/app/plataforma/plataforma-layout.tsx

--- tigerjus/src/app/plataforma/plataforma-layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AppSettingsProvider } from '@/contexts/AppSettingsContext'
-
-export default function PlataformaLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <AppSettingsProvider>
-      {children}
-    </AppSettingsProvider>
-  )
-}
